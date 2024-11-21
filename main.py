1991849077044e059c26ea6f9781c062fab55ad9
Zohnnine <yppmusic@gmail.com>
2024-11-21 12:58:14 +0900

    1121 sidebar revised

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from supabase import create_client, Client
 
 # page setup
 st.set_page_config(layout="wide")
-st.logo("./assets/sidebar_logo_01.png",size="large")
+st.logo("./assets/sidebar_revised.png",size="large")
 @st.cache_resource
 def init_connection():
     url = st.secrets["SUPABASE_URL"]
