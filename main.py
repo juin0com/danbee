fc35886ed836eae0dcce3b7ab206376eae036c06
Zohnnine <yppmusic@gmail.com>
2024-11-21 11:33:49 +0900

    1121 side bar logo image update (en)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from supabase import create_client, Client
 
 # page setup
 st.set_page_config(layout="wide")
-st.logo("./assets/logo12.png",size="large")
+st.logo("./assets/danbee_sidebar_logo.jpg",size="large")
 @st.cache_resource
 def init_connection():
     url = st.secrets["SUPABASE_URL"]
